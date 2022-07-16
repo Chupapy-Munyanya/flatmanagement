4420a51b21ec08d20f1ccd78df0d9463584d0efc
through-your-tears <warstream55@gmail.com>
2022-07-16 09:45:44 +0300

    Добавлены эндпоинты

--- flatmanagment/flats/urls.py
+++ flatmanagment/flats/urls.py
@@ -12,5 +12,8 @@ urlpatterns = [
     path('buildingcompany/<int:company_id>/', BuildingCompanyAPIView.as_view()),
     path('apartments/', ApartmentsAPIView.as_view()),
     path('apartment/<int:apart_id>/', ApartmentViewAPI.as_view()),
-    path('house/<int:house_id>/', HouseAPIView.as_view())
+    path('house/<int:house_id>/', HouseAPIView.as_view()),
+    path('flat/<int:house_id>/', FlatsAPIView.as_view()),
+    path('commercial/<int:house_id>/', CommercialsAPIView.as_view()),
+    path('type/<int:house_id>/', FlatTypeAPIView.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
